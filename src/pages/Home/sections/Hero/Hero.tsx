abbd714af177acbec1918dafd59ba46b8f5b24de
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-09-20 09:44:54 -0300

    Instaling material icons

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,44 +1,47 @@
 
-import { Container, Grid, styled } from '@mui/material'
+import { Button, Container, Grid, styled, Typography } from '@mui/material'
 import Avatar from '../../../../assets/Avatar.png'
-
+import DownloadIcon from '@mui/icons-material/Download';
+import EmailIcon from '@mui/icons-material/Email';
 const Hero = () => {
 
   const StyledHero = styled('div') (() => ({
     backgroundColor: 'red',
-    color: 'white',
-    padding: '1rem',
-    textAlign: 'center',
-    height: '100vh'
+    height: '100vh',
   }))
 
   const StyledImg = styled('img') (() => ({
-    width: '100px',
-    height: '100px',
+    width: '100%',
     borderRadius: '50%',
     objectFit: 'cover'
     
   }))
   return (
     <>
-    <Container maxWidth="lg">
-      <Grid container spacing={1}>
-        <Grid item xs={4} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-        <StyledImg src={Avatar} alt="Avatar"/>
-        </Grid>
-        <Grid item xs={8} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-          xs=4
-        </Grid>
-      </Grid>
-    </Container>
-
       <StyledHero>
-        
-        <h1>Anthony Steffen</h1>
-        <p>Im a Web Developer full Stack</p>
-   
-  
+        <Container maxWidth="lg">
+          <Grid container spacing={2}> 
+            <Grid item xs={12} md={4}>
+            <StyledImg src={Avatar} alt="Avatar"/>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant="h1" textAlign={'center'} color='white'>Anthony Steffen</Typography>
+              <Typography variant="h2" textAlign={'center'} color='white'>Im a Web Developer full Stack</Typography>
+             
+                <Button>
+                <DownloadIcon/>
+                  Download CV
+                </Button>
+                <Button>
+                <EmailIcon/>
+                  Contact me
+                </Button>
+             
+            </Grid>
+          </Grid>
+        </Container>
       </StyledHero>
+
 
     </>
   )
