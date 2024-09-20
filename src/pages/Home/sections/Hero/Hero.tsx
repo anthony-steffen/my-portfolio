aaa1b4b79020d96600370c6e9ae2495569124a14
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-09-20 15:11:27 -0300

    feat: configuring material with css baseline and inserting responsive meta tag

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,50 +1,54 @@
-
-import { Button, Container, Grid, styled, Typography } from '@mui/material'
-import Avatar from '../../../../assets/Avatar.png'
-import DownloadIcon from '@mui/icons-material/Download';
-import EmailIcon from '@mui/icons-material/Email';
+import { Button, Container, Grid, styled, Typography } from "@mui/material";
+import Avatar from "../../../../assets/Avatar.png";
+import DownloadIcon from "@mui/icons-material/Download";
+import EmailIcon from "@mui/icons-material/Email";
 const Hero = () => {
+	const StyledHero = styled("div")(() => ({
+		backgroundColor: "red",
+		height: "100vh",
+	}));
 
-  const StyledHero = styled('div') (() => ({
-    backgroundColor: 'red',
-    height: '100vh',
-  }))
+	const StyledImg = styled("img")(() => ({
+		width: "100%",
+		borderRadius: "50%",
+		objectFit: "cover",
+	}));
+	return (
+		<>
+			<StyledHero>
+				<Container maxWidth="lg">
+					<Grid container spacing={2}>
+						<Grid item xs={12} md={4}>
+							<StyledImg src={Avatar} alt="Avatar" />
+						</Grid>
+						<Grid item xs={12} md={8}>
+							<Typography variant="h1" textAlign={"center"} color="white">
+								Anthony Steffen
+							</Typography>
+							<Typography variant="h2" textAlign={"center"} color="white">
+								Im a Web Developer full Stack
+							</Typography>
 
-  const StyledImg = styled('img') (() => ({
-    width: '100%',
-    borderRadius: '50%',
-    objectFit: 'cover'
-    
-  }))
-  return (
-    <>
-      <StyledHero>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}> 
-            <Grid item xs={12} md={4}>
-            <StyledImg src={Avatar} alt="Avatar"/>
-            </Grid>
-            <Grid item xs={12} md={8}>
-              <Typography variant="h1" textAlign={'center'} color='white'>Anthony Steffen</Typography>
-              <Typography variant="h2" textAlign={'center'} color='white'>Im a Web Developer full Stack</Typography>
-             
-                <Button>
-                <DownloadIcon/>
-                  Download CV
-                </Button>
-                <Button>
-                <EmailIcon/>
-                  Contact me
-                </Button>
-             
-            </Grid>
-          </Grid>
-        </Container>
-      </StyledHero>
+							<Grid container spacing={2} display={"flex"} justifyContent={"center"}>
+								<Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+									<Button>
+										<DownloadIcon />
+										Download CV
+									</Button>
+								</Grid>
+								<Grid item xs={12} md={4} display={"flex"} justifyContent={"center"}>
+									<Button>
+										<EmailIcon />
+										Contact me
+									</Button>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Container>
+			</StyledHero>
+		</>
+	);
+};
 
-
-    </>
-  )
-}
-
-export default Hero
+export default Hero;
