517b65541663e90bb827a8fd3693b1f29a2cf583
Anthony Steffen <anthony.steffen@outlook.com.br>
2024-09-29 10:38:13 -0300

    :art: style: Standardizing Background of Pages

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,16 +1,42 @@
-import {styled} from '@mui/material'
+import {styled, Typography} from '@mui/material'
+import Bg from '../../../../assets/vecteezy_virtual-tecnology-blue-vector-background-eps-10_10742634.jpg'; // Importando a imagem de fundo
 
 const About: React.FC = () => {
-  const StyledAbout = styled("section")(() => ({
-    backgroundColor: 'green',
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
- 
+  const StyledAbout = styled('div')(({ theme }) => ({
+    backgroundImage: `url(${Bg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    color: theme.palette.primary.contrastText,
+    
+
+    [theme.breakpoints.up('md')]: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem',
+      height: '100vh',
+    },
+    [theme.breakpoints.down('md')]: {
+      paddingTop: '2rem',
+      paddingBottom: '1rem',
+      height: '100vh',
+    },
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem',
+      height: '100%',
+    },
   }));
+ 
   return (
     <StyledAbout>
-      About
+      <Typography 
+      variant="h2" 
+      align="center" 
+      gutterBottom
+      style={{paddingTop: '5rem'}}
+      >
+      About me
+      </Typography>
     </StyledAbout>
   )
 }
--- src/pages/Home/sections/Contact/Contact.tsx
+++ src/pages/Home/sections/Contact/Contact.tsx
@@ -1,15 +1,41 @@
-import {styled} from '@mui/material'
+import {styled, Typography} from '@mui/material'
+import Bg from '../../../../assets/vecteezy_virtual-tecnology-blue-vector-background-eps-10_10742634.jpg'; // Importando a imagem de fundo
 
 const Contact = () => {
-  const StyledContact = styled("section")(() => ({
-    backgroundColor: 'blue',
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
+  const StyledContact = styled("section")(({theme}) => ({
+    backgroundImage: `url(${Bg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    color: theme.palette.primary.contrastText,
+    
+
+    [theme.breakpoints.up('md')]: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem',
+      height: '100vh',
+    },
+    [theme.breakpoints.down('md')]: {
+      paddingTop: '2rem',
+      paddingBottom: '1rem',
+      height: '100vh',
+    },
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: '1rem',
+      paddingBottom: '1rem',
+      height: '100%',
+    },
   }));
   return (
     <StyledContact>
-      Contact
+      <Typography 
+      variant="h2" 
+      align="center" 
+      gutterBottom
+      style={{paddingTop: '5rem'}}
+      >
+      Contact me
+      </Typography>
     </StyledContact>
   )
 }
